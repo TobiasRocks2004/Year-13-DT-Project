7a0021e76ba145014216ef63e784b9185bb90e8f
Tobias Willis <tobiasdwillis@gmail.com>
2022-02-24 11:26:49 +1300

    Added base of the project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public Transform groundCheck;
+    public float groundDist = 0.3f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    public float speed = 12f;
+    public float gravity = -9.8f;
+
+    Vector3 velocity;
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        if (move.magnitude > 1)
+        {
+            move /= move.magnitude;
+        }
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
