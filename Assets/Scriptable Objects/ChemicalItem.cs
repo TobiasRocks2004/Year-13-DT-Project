6f1259248d33bb4984f9148687534fdfe7a85acc
TobiasRocks2004 <tobiasdwillis@gmail.com>
2022-03-04 13:41:46 +1300

    Added some extra effects and some groundwork for the chemicals

---
+++ Assets/Scriptable Objects/ChemicalItem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Chemical Item", menuName = "Chemical")]
+public class ChemicalItem : ScriptableObject
+{
+    public GameObject prefab;
+}
